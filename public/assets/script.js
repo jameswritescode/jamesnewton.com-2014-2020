c58ccfd0b89daa12a014bb3706d108d9d44af48e
James Newton <hello@jamesnewton.com>
2015-08-27 23:37:01 -0400

    ADD SOME SUPER AWESOME JAVASCRIPT YEAH!

---
+++ public/assets/script.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('.js-typed').typed({
+    strings:   ['a cat owner', 'a wantrepreneur', 'a tamagotchi', 'a musician',
+                'a schmuck', 'a drummer', 'an emoji enthusiast',
+                'a software developer'],
+    backDelay: 1000
+  });
+});
