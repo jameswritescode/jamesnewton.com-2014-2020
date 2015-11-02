a5234b537f2dec4a7243b9f3d84df8cec8352d57
James Newton <hello@jamesnewton.com>
2015-11-01 23:13:17 -0500

    add css for section#blog img

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,3 +6,35 @@ $(function() {
     backDelay: 1000
   });
 });
+
+// TIP: B A
+var secret = '38384040373937396665',
+    input  = '',
+    konamis = [greatBattleOfCat, justDOIT],
+    timer;
+
+function greatBattleOfCat() {
+  alert('Nyan Cat vs. Tac Nayn');
+}
+
+function justDOIT() {
+  alert('JUST DO IT');
+}
+
+function checkInput() {
+  if (input.indexOf(secret) > -1) {
+    konamis[Math.floor(Math.random() * konamis.length)]();
+  }
+}
+
+$(document).keyup(function(e) {
+  input += e.which;
+
+  clearTimeout(timer);
+
+  timer = setTimeout(function() {
+    input = ''
+  }, 500);
+
+  checkInput();
+});
