fe5648eedfdec2ac361a3d97130976a47bca0aa5
James Newton <hello@jamesnewton.com>
2016-02-24 10:44:48 -0500

    habitual line stepper has been used to describe me

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,7 @@
 $(function() {
   $('.js-typed').typed({
     strings:   ['a cat owner', 'a wantrepreneur', 'a tamagotchi', 'a musician',
-                'a schmuck', 'a drummer', 'an emoji enthusiast',
+                'a habitual line stepper', 'a drummer', 'an emoji enthusiast',
                 'a software developer'],
     backDelay: 1000
   });
